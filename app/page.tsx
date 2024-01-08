ba9d0fae077b79b417e1c597507cc8891bfd870b
peter varga <pkvarg@yahoo.se>
2024-01-08 10:47:11 +0100

    home mob

--- app/page.tsx
+++ app/page.tsx
@@ -7,42 +7,42 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="hero h-[90vh] text-white lg:h-[100vh] ">
+    <main className="hero h-[100vh] text-white lg:h-[100vh] ">
       <Navbar />
-      <div className="mx-[18%] grid translate-y-1/2 transform grid-cols-3 gap-4 text-[25px] font-thin uppercase">
+      <div className="mx-4 flex transform flex-col   gap-4 text-[25px] font-thin uppercase lg:mx-[18%] lg:grid lg:translate-y-1/2 lg:grid-cols-3">
         <div
           onClick={() => router.push('/preprava')}
-          className="hover:text-underline flex h-[150px] cursor-pointer items-center justify-center bg-[#86747867] hover:border hover:border-yellow-400 hover:text-yellow-400"
+          className="hover:text-underline flex h-[80px] cursor-pointer items-center justify-center bg-[#86747867] hover:border hover:border-yellow-400 hover:text-yellow-400 lg:h-[150px]"
         >
           Preprava áut
         </div>
         <div
           onClick={() => router.push('/odtahovka')}
-          className="hover:text-underline flex h-[150px] cursor-pointer items-center justify-center bg-[#86747867] hover:border  hover:border-yellow-400 hover:text-yellow-400"
+          className="hover:text-underline flex h-[80px] cursor-pointer items-center justify-center bg-[#86747867] hover:border  hover:border-yellow-400 hover:text-yellow-400 lg:h-[150px]"
         >
           Odťahová služba
         </div>
         <div
           onClick={() => router.push('/transfer')}
-          className="hover:text-underline flex h-[150px] cursor-pointer items-center justify-center  bg-[#86747867] hover:border hover:border-yellow-400 hover:text-yellow-400"
+          className="hover:text-underline flex h-[80px] cursor-pointer items-center justify-center  bg-[#86747867] hover:border hover:border-yellow-400 hover:text-yellow-400 lg:h-[150px]"
         >
           Letiskový transfer
         </div>
         <div
           onClick={() => router.push('/prenajom')}
-          className="hover:text-underline flex h-[150px] cursor-pointer items-center justify-center bg-[#86747867] hover:border  hover:border-yellow-400 hover:text-yellow-400"
+          className="hover:text-underline flex h-[80px] cursor-pointer items-center justify-center bg-[#86747867] hover:border hover:border-yellow-400  hover:text-yellow-400 lg:h-[150px]"
         >
           Prenájom vozidiel
         </div>
         <div
           onClick={() => router.push('/servis')}
-          className="hover:text-underline flex h-[150px] cursor-pointer items-center justify-center bg-[#86747867] hover:border  hover:border-yellow-400 hover:text-yellow-400"
+          className="hover:text-underline flex h-[80px] cursor-pointer items-center justify-center bg-[#86747867] hover:border hover:border-yellow-400  hover:text-yellow-400 lg:h-[150px]"
         >
           Servis
         </div>
         <div
           onClick={() => router.push('/predaj-vykup')}
-          className="hover:text-underline flex h-[150px] cursor-pointer items-center justify-center bg-[#86747867] hover:border  hover:border-yellow-400 hover:text-yellow-400"
+          className="hover:text-underline flex h-[80px] cursor-pointer items-center justify-center bg-[#86747867] hover:border hover:border-yellow-400  hover:text-yellow-400 lg:h-[150px]"
         >
           Predaj a výkup
         </div>
